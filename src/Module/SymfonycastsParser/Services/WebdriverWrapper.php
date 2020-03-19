cdafd9bb972e932f58117d6fba440b7c2731b980
glaphire <motoryna.dariia@pdffiller.team>
2020-03-19 23:06:51 +0200

    moving logic to webdriver wrapper

---
+++ src/Module/SymfonycastsParser/Services/WebdriverWrapper.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Module\SymfonycastsParser\Services;
+
+use Facebook\WebDriver\Chrome\ChromeOptions;
+use Facebook\WebDriver\Remote\DesiredCapabilities;
+use Facebook\WebDriver\Remote\RemoteWebDriver;
+use Facebook\WebDriver\WebDriverBy;
+use Facebook\WebDriver\WebDriverExpectedCondition;
+
+class WebdriverWrapper
+{
+    private $webdriver;
+
+    public function __construct(string $host, string $downloadDefaultDirectoryAbsPath)
+    {
+        $options = new ChromeOptions();
+        $options->setExperimentalOption("prefs", [
+            "download.prompt_for_download" => false,
+            "download.directory_upgrade" => true,
+            "safebrowsing.enabled" => true,
+            "download.default_directory" => $downloadDefaultDirectoryAbsPath,
+            "plugins.always_open_pdf_externally" => true,
+        ]);
+
+        $caps = DesiredCapabilities::chrome();
+        $caps->setCapability(ChromeOptions::CAPABILITY, $options);
+        $this->webdriver = RemoteWebDriver::create($host, $caps);
+    }
+
+    public function click(string $cssSelector)
+    {
+        $this
+            ->webdriver
+            ->findElement(WebDriverBy::cssSelector($cssSelector))
+            ->click();
+    }
+
+    public function waitToBeClickable(string $cssSelector)
+    {
+        $this->webdriver->wait()->until(
+            WebDriverExpectedCondition::elementToBeClickable(WebDriverBy::cssSelector($cssSelector))
+        );
+    }
+
+    public function waitAndClick(string $cssSelector)
+    {
+        $this->waitToBeClickable($cssSelector);
+        $this->click($cssSelector);
+    }
+}
