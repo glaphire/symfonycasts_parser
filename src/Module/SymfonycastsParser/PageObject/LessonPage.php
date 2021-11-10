5a6a29a237118d22030a93a63b8c9e55d39218ec
glaphire <daria.motorina@gmail.com>
2021-11-10 21:33:52 +0200

    refactored LessonPage

--- src/Module/SymfonycastsParser/PageObject/LessonPage.php
+++ src/Module/SymfonycastsParser/PageObject/LessonPage.php
@@ -6,12 +6,12 @@ use App\Module\SymfonycastsParser\WebdriverFacade\WebdriverFacadeInterface;
 
 class LessonPage extends AbstractPageObject
 {
-    public const DOWNLOAD_MENU_BUTTON = '#downloadDropdown';
-    public const DOWNLOAD_MENU_LIST = '.dropdown-menu.show';
+    public const SLCTR_DOWNLOAD_MENU_BUTTON = '#downloadDropdown';
+    public const SLCTR_DOWNLOAD_MENU_LIST = '.dropdown-menu.show';
 
-    public const DOWNLOAD_MENU_LIST_ITEM_VIDEO = '.dropdown-menu a[data-download-type=video]';
-    public const DOWNLOAD_MENU_LIST_ITEM_SCRIPT = '.dropdown-menu a[data-download-type=script]';
-    public const DOWNLOAD_MENU_LIST_ITEM_CODE_ARCHIVE = '.dropdown-menu a[data-download-type=code]';
+    public const SLCTR_DOWNLOAD_MENU_LIST_ITEM_VIDEO = '.dropdown-menu a[data-download-type=video]';
+    public const SLCTR_DOWNLOAD_MENU_LIST_ITEM_SCRIPT = '.dropdown-menu a[data-download-type=script]';
+    public const SLCTR_DOWNLOAD_MENU_LIST_ITEM_CODE_ARCHIVE = '.dropdown-menu a[data-download-type=code]';
 
     public function __construct(WebdriverFacadeInterface $webdriver)
     {
@@ -24,40 +24,40 @@ class LessonPage extends AbstractPageObject
         $parseCodeArchiveFlag = false
     ) {
         if ($parseVideoFlag) {
-            $this->clickDropdownOptionAndDownload(self::DOWNLOAD_MENU_LIST_ITEM_VIDEO);
+            $this->clickDropdownOptionAndDownload(self::SLCTR_DOWNLOAD_MENU_LIST_ITEM_VIDEO);
         }
         if ($parseScriptFlag) {
-            $this->clickDropdownOptionAndDownload(self::DOWNLOAD_MENU_LIST_ITEM_SCRIPT);
+            $this->clickDropdownOptionAndDownload(self::SLCTR_DOWNLOAD_MENU_LIST_ITEM_SCRIPT);
         }
         if ($parseCodeArchiveFlag) {
-            $this->clickDropdownOptionAndDownload(self::DOWNLOAD_MENU_LIST_ITEM_CODE_ARCHIVE);
+            $this->clickDropdownOptionAndDownload(self::SLCTR_DOWNLOAD_MENU_LIST_ITEM_CODE_ARCHIVE);
         }
 
-        $this->webdriver->waitFilesToDownload();
+        $this->webdriver->waitFilesToDownload(); //TODO: recheck if this line is neccessary
     }
 
     public function downloadVideo()
     {
-        $this->clickDropdownOptionAndDownload(self::DOWNLOAD_MENU_LIST_ITEM_VIDEO);
+        $this->clickDropdownOptionAndDownload(self::SLCTR_DOWNLOAD_MENU_LIST_ITEM_VIDEO);
         $this->webdriver->waitFilesToDownload();
     }
 
     public function downloadCourseScript()
     {
-        $this->clickDropdownOptionAndDownload(self::DOWNLOAD_MENU_LIST_ITEM_SCRIPT);
+        $this->clickDropdownOptionAndDownload(self::SLCTR_DOWNLOAD_MENU_LIST_ITEM_SCRIPT);
         $this->webdriver->waitFilesToDownload();
     }
 
     public function downloadCourseCodeArchive()
     {
-        $this->clickDropdownOptionAndDownload(self::DOWNLOAD_MENU_LIST_ITEM_CODE_ARCHIVE);
+        $this->clickDropdownOptionAndDownload(self::SLCTR_DOWNLOAD_MENU_LIST_ITEM_CODE_ARCHIVE);
         $this->webdriver->waitFilesToDownload();
     }
 
     protected function clickDropdownOptionAndDownload(string $cssSelector)
     {
-        $this->webdriver->waitAndClick(self::DOWNLOAD_MENU_BUTTON);
-        $this->webdriver->waitToBeClickable(self::DOWNLOAD_MENU_LIST);
+        $this->webdriver->waitAndClick(self::SLCTR_DOWNLOAD_MENU_BUTTON);
+        $this->webdriver->waitToBeClickable(self::SLCTR_DOWNLOAD_MENU_LIST);
         $this->webdriver->click($cssSelector);
     }
 }
